056774dba73944c184d8a59d6d7bbcec0fec6200
lcw99 <changlee99@gmail.com>
2024-11-09 19:41:07 +0900

    aa

--- compress-saju-int8.py
+++ compress-saju-int8.py
@@ -1,13 +1,18 @@
+import argparse
 from datasets import load_dataset, load_from_disk
 from transformers import AutoTokenizer
-
 from llmcompressor.modifiers.quantization import GPTQModifier
 from llmcompressor.transformers import SparseAutoModelForCausalLM, oneshot
 from llmcompressor.transformers.compression.helpers import calculate_offload_device_map
 
+# Parse command line arguments
+parser = argparse.ArgumentParser(description="Quantize a model with GPTQ.")
+parser.add_argument("--model", type=str, required=True, help="Path to the model directory.")
+args = parser.parse_args()
+
 # 1) Select model and load it.
-MODEL_ID = "/home/chang/t9/release-models/google-gemma-2-9b-saju-240906"
-device_map = calculate_offload_device_map(MODEL_ID, reserve_for_hessians=True, num_gpus=1)
+MODEL_ID = args.model
+device_map = calculate_offload_device_map(MODEL_ID, reserve_for_hessians=True, num_gpus=4)
 model = SparseAutoModelForCausalLM.from_pretrained(
     MODEL_ID,
     device_map=device_map,
@@ -16,13 +21,12 @@ model = SparseAutoModelForCausalLM.from_pretrained(
 tokenizer = AutoTokenizer.from_pretrained(MODEL_ID)
 
 # 2) Prepare calibration dataset.
-DATASET_ID = "/home/chang/AI/llm/autoawq/calib_data/dataset/train"
+DATASET_ID = "/home/chang/t9/Models/calib_data_gemma/dataset/train"
 DATASET_SPLIT = "train_sft"
 
 # Select number of samples. 512 samples is a good place to start.
-# Increasing the number of samples can improve accuracy.
-NUM_CALIBRATION_SAMPLES = 4096
-MAX_SEQUENCE_LENGTH = 2048
+NUM_CALIBRATION_SAMPLES = 1024
+MAX_SEQUENCE_LENGTH = 1024
 
 # Load dataset and preprocess.
 ds = load_from_disk(DATASET_ID)
@@ -54,7 +58,6 @@ ds = ds.map(tokenize, remove_columns=ds.column_names)
 # 3) Select quantization algorithms. In this case, we:
 #   * quantize the weights to int8 with GPTQ (static per channel)
 #   * quantize the activations to int8 (dynamic per token)
-# Note: set sequential_update: true in the recipe to reduce memory
 recipe = GPTQModifier(sequential_update=True, targets="Linear", scheme="W8A8", ignore=["lm_head"])
 
 # 4) Apply quantization and save to disk compressed.
@@ -64,7 +67,7 @@ oneshot(
     recipe=recipe,
     max_seq_length=MAX_SEQUENCE_LENGTH,
     num_calibration_samples=NUM_CALIBRATION_SAMPLES,
-    output_dir=MODEL_ID + "-int8",
+    output_dir=MODEL_ID + "/int8",
 )
 
 # Confirm generations of the quantized model look sane.
@@ -73,3 +76,6 @@ input_ids = tokenizer("Hello my name is", return_tensors="pt").input_ids.to("cud
 output = model.generate(input_ids, max_new_tokens=20)
 print(tokenizer.decode(output[0]))
 print("==========================================")
+
+
+
